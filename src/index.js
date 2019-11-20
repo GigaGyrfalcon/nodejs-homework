946272b04f99bd3e477d2836fab499720f5f8c47
Giga Songulashvili <giga_songulashvili@epam.com>
2019-11-20 14:56:51 +0100

    Homework 1 task 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Nodejs runs");
+var str_reverse = require('./str_reverse');
+
+var reversed1 = str_reverse('123456 78');
+console.log(reversed1, `\n`);
+
+var reversed2 = str_reverse('test data');
+console.log(reversed2, `\n`);
---
+++ src/str_reverse.js
@@ -0,0 +1,8 @@
+function str_reverse(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+
+module.exports = str_reverse;
