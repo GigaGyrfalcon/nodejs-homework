a0e7fc35ec0615099e354e36f37be093df51ce44
Giga Songulashvili <giga_songulashvili@epam.com>
2019-11-22 10:43:48 +0100

    Update task 1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-// Task 1
-import str_reverse from './str_reverse';
-
-var reversed1 = str_reverse('123456 78');
-console.log(reversed1);
-
-var reversed2 = str_reverse('test data');
-console.log(reversed2);
-
 // Task 2
+
 import fs from 'fs';
 import { convertCSVtoJSON, convertCSVtoJSONStream } from './csv-to-json';
 
